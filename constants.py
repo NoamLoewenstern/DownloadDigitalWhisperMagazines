78a7f3d0eec6c1ceb0d689223992fa1a1d35cd9d
Noam Loewenstern <Noam.Loewenstern@gmail.com>
2020-03-09 21:42:40 +0200

    works 80%, has errors.

--- constants.py
+++ constants.py
@@ -1,10 +1,12 @@
 from os.path import realpath, dirname
+from pathlib import Path
 TABLE_XPATH = r'//*[@id="content"]/div/div[1]/span/div/table/tbody'
 FULL_SHEET_XPATH = r'//*[@id="content"]/div/div[1]/div[1]/a'
-DEBUG = True
+DEBUG = False
 NUM_SHEETS = 121 if not DEBUG else 1
-CUR_DIR = dirname(realpath(__file__))
+CUR_DIR = Path(dirname(realpath(__file__)))
 
 BASE_URL = r'https://www.digitalwhisper.co.il/issue{num_sheet}'
-LINK_REGEX_HREF_CONTENT = r'http://www.digitalwhisper.co.il/files/Zines/0x\d+?/.+?\.pdf'
-DST_MAGAZINES = 'all_magazines'
+LINK_REGEX_HREF_CONTENT = r'(http://www.digitalwhisper.co.il|\.\.)/files/Zines/0x(\d|[a-fA-F]){0,4}/.+?\.pdf'
+DST_MAGAZINES = CUR_DIR / 'all_magazines'
+DST_MAGAZINES_JSON = DST_MAGAZINES / 'magazines.json'
--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import re
 import os
-from os.path import join
+import json
 from urllib.request import urlopen
-from lxml import etree
+import urllib
+import logging
 from bs4 import BeautifulSoup
 
 from constants import TABLE_XPATH, FULL_SHEET_XPATH, DEBUG, NUM_SHEETS, \
-    DST_MAGAZINES, BASE_URL, CUR_DIR, LINK_REGEX_HREF_CONTENT
+    DST_MAGAZINES, BASE_URL, CUR_DIR, LINK_REGEX_HREF_CONTENT, DST_MAGAZINES_JSON
+
+
+def out_temp(data='', path=CUR_DIR / 'temp.html'):
+    try:
+        path.write_text(data)
+    except:
+        path.write_bytes(data)
+
+
+def setup():
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+    if not DST_MAGAZINES.is_dir():
+        os.mkdir(DST_MAGAZINES)
 
 def get_all_magazines_links_to_download() -> dict:
     magazines = {}
-    for num_sheet in range(1, NUM_SHEETS + 1):
+    for num_sheet in range(1, 11): #NUM_SHEETS + 1):
+        logging.info(f"{num_sheet=}")
         url = BASE_URL.format(num_sheet=num_sheet)
-        response = urlopen(url)
-        data = response.read()
+        try:
+            response = urlopen(url)
+        except Exception as err:
+            logging.error(err)
+            continue
+        data = response.read() # out_temp(data)
         soup = BeautifulSoup(data, features="lxml")
 
         all_elems = soup.find_all('a',
                                   href=re.compile(LINK_REGEX_HREF_CONTENT))
+        logging.debug(f"len(all_elems) = {len(all_elems)}")
+        if not all_elems:
+            logging.info('HAS NO ELEMENTS: all_elems')
+            continue
         href = all_elems[0].attrs['href']
         fname = href.split('/')[-1]
         magazines[num_sheet] = [(href, fname)]
@@ -27,22 +50,32 @@ def get_all_magazines_links_to_download() -> dict:
             magazines[num_sheet].append((href, fname))
     return magazines
 
+
+def dump_magazines(magazines: dict) -> None:
+    DST_MAGAZINES_JSON.write_text(json.dumps(magazines, sort_keys=True))
+
 def download_all_magazines(magazines: dict) -> None:
-    for num_sheet, values in magazines.items():
-        dirpath = join(CUR_DIR, DST_MAGAZINES, str(num_sheet))
-        if not os.path.isdir(dirpath):
+    for num_sheet in sorted(int(key) for key in magazines):
+        logging.info(f"{num_sheet=}")
+        values = magazines[str(num_sheet)]
+        dirpath = DST_MAGAZINES / str(num_sheet)
+        if not dirpath.is_dir():
             os.mkdir(dirpath)
         for href, fname in values:
-            with open(join(dirpath, fname), 'wb') as out_fh:
-                resp = urlopen(href)
-                pdf_data = resp.read()
-                out_fh.write(pdf_data)
+            try:
+                with (dirpath / fname).open('wb') as out_fh:
+                    resp = urlopen(href)
+                    pdf_data = resp.read()
+                    out_fh.write(pdf_data)
+            except Exception as err:
+                logging.error(err)
 
 
 def main():
-    if os.path.isdir(join(CUR_DIR, DST_MAGAZINES)):
-        os.mkdir(join(CUR_DIR, DST_MAGAZINES))
-    magazines = get_all_magazines_links_to_download()
+    setup()
+    # magazines = get_all_magazines_links_to_download()
+    # dump_magazines(magazines)
+    magazines = json.loads(DST_MAGAZINES_JSON.read_bytes().decode(), encoding='utf8')
     download_all_magazines(magazines)
 
 
